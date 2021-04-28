33a2a5daea29aa5a8bb44e6a48456d6104814af5
Arnaud Lalouette <arnaud.lalouette@its4u.com>
2021-04-28 17:04:50 +0200

    Initial version

--- src/main/java/com/its4u/rest/resources/TestResource.java
+++ src/main/java/com/its4u/rest/resources/TestResource.java
@@ -1,15 +1,25 @@
 package com.its4u.rest.resources;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 @Path("/test")
+@Consumes(MediaType.TEXT_PLAIN)
+@Produces(MediaType.TEXT_PLAIN)
 public class TestResource {
+
+	@PersistenceContext(unitName = "IAMPU")
+	private EntityManager iamEntityManager;
 
 	@GET
 	public Response test() {
-		return Response.ok().build();
+		return Response.ok(iamEntityManager.toString()).build();
 	}
 
 }
